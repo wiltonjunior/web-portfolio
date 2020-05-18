0b4a6d6d95db2d482e8d70f2bc8e9b87756a9b51
Wilton Júnior <wiltonjunior2010@gmail.com>
2020-05-17 22:12:48 -0300

    fixed: add carrocel in project

--- src/shared/components/Cases/components/CardCase/index.jsx
+++ src/shared/components/Cases/components/CardCase/index.jsx
@@ -1,28 +1,35 @@
 import React from "react"
 
+import Button from "@components/Button"
+
 import "./styles.scss"
 
 const CardCase = ({ item, active }) => {
   const { title, description, articles = [] } = item || {}
   return (
     <div className={`CardCase ${active ? "active" : ""}`}>
-      <div className="column">
-        <div className="card_image"></div>
-      </div>
-      <div className="column">
-        <div className="card_description">
-          <div className="header">
-            <h1 className="title">{title}</h1>
-            <p className="description">{description}</p>
-          </div>
-          <div className="colunm-body">
-            {articles.map((item, index) => (
-              <p key={index} className="article">
-                {item.description}
-              </p>
-            ))}
+      <div className="card_body">
+        <div className="column">
+          <div className="card_image"></div>
+        </div>
+        <div className="column">
+          <div className="card_description">
+            <div className="header">
+              <h1 className="title">{title}</h1>
+              <p className="description">{description}</p>
+            </div>
+            <div className="colunm-body">
+              {articles.map((item, index) => (
+                <p key={index} className="article">
+                  {item.description}
+                </p>
+              ))}
+            </div>
           </div>
         </div>
+      </div>
+      <div className="card_footer">
+        <Button basic>Acessar o Case</Button>
       </div>
     </div>
   )
