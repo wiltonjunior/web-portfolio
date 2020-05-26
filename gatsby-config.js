e84d117148e50009e2c6e2a1d344159f6cc1d4e3
Wilton Júnior <wiltonjunior2010@gmail.com>
2020-05-26 18:30:46 -0300

    fixed: add plugin gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     {
@@ -49,8 +49,6 @@ module.exports = {
         }
       }
     },
-    // `gatsby-plugin-offline`,
-    // To learn more, visit: https://gatsby.dev/offline
-    // this (optional) plugin enables Progressive Web App + Offline functionality
+    `gatsby-plugin-offline`,
   ],
 }
