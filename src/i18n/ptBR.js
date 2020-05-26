740bf90bd64cd22a9df55947d007eed366b1e4f4
Wilton Júnior <wiltonjunior2010@gmail.com>
2020-05-26 18:47:42 -0300

    feat: add header translater

--- src/i18n/ptBR.js
+++ src/i18n/ptBR.js
@@ -1,3 +1,7 @@
 export default {
-  LANG: 'pt-BR',
+  LANG: "pt-BR",
+  HEADER_ITEM_TITLE_HOME: "Home",
+  HEADER_ITEM_TITLE_ABOUT: "Sobre",
+  HEADER_ITEM_TITLE_CASES: "Cases",
+  HEADER_ITEM_TITLE_CONTACT: "Contato",
 }
