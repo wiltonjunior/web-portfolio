50b05dd35943dc99879ac0bda2880cc567cf76cf
Wilton Júnior <wiltonjunior2010@gmail.com>
2020-05-29 21:28:38 -0300

    fixed: add tranlater carde case

--- src/shared/components/Cases/components/CardCase/index.jsx
+++ src/shared/components/Cases/components/CardCase/index.jsx
@@ -31,7 +31,7 @@ const CardCase = ({ item, active }) => {
         </div>
       </div>
       <div className="card_footer">
-        <Button basic href={item.link}>Acessar o Case</Button>
+        <Button basic href={item.link}>ACESS_CASE</Button>
       </div>
     </div>
   )
